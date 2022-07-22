a0ccc77f9610fc7ee6fd4b8c860da5ca02307f30
Maszto071 <tpodgorski@mmm.com>
2022-07-22 15:19:20 +0200

    another useful comment

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
 import random
 
 def multiply(x):
+    # first-class usage
     return lambda n:n*x
+
+def power(x):
+    #another one
+    return lambda n:n**x
 
 def create_3_ele_list(random_l:list):
     for i in range(3):
         random_l.append(random.random())
     return random_l
 
-def multiply_all_ele():
-    list(map(first_class, random_l))
+def apply_f_c_fnc(fnc):
+    return list(map(fnc, random_l))
 
 
 if __name__ == '__main__':
-    first_class = multiply(2)
+    multiply_do = multiply(2)
+    power_do = power(2)
     random_l = create_3_ele_list([])
-    multiply_all_ele()
-    print(random_l)
+    mulit_l = apply_f_c_fnc(multiply_do)
+    power_l = apply_f_c_fnc(power_do)
+    print(mulit_l, power_l)
